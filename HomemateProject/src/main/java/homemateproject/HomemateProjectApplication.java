bbe54c5ca5262b810d3aacadc5ebdbb0a721477f
ATuan20 <tuanla267@gmail.com>
2023-09-29 16:04:04 +0700

    khung project

---
+++ HomemateProject/src/main/java/homemateproject/HomemateProjectApplication.java
@@ -0,0 +1,13 @@
+package homemateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomemateProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomemateProjectApplication.class, args);
+    }
+
+}
---
+++ HomemateProject/src/test/java/homemateproject/HomemateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package homemateproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemateProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
